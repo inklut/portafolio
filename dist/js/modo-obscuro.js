2082a5442d5581997caabae7de5328183ac75e98
garyavendanio <avenros1@gmail.com>
2021-07-09 16:05:31 -0500

    ./*.*

--- dist/js/modo-obscuro.js
+++ dist/js/modo-obscuro.js
@@ -9,3 +9,20 @@ const toggleDarkMode = function () {
 
 toggleDarkMode();
 checkbox.addEventListener('click', toggleDarkMode);
+
+
+if (localStorage.toggleDarkMode === 'dark' || (!('checkbox' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    html.documentElement.classList.add('dark')
+} else {
+    html.documentElement.classList.remove('dark')
+}
+  
+// Whenever the user explicitly chooses light mode
+localStorage.toggleDarkMode = 'light'
+  
+// Whenever the user explicitly chooses dark mode
+localStorage.toggleDarkMode = 'dark'
+  
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('checkbox')
+
