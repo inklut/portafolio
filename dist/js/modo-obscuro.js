39af77e802a7bc622d92f952b30452b9b92cc146
garyavendanio <avenros1@gmail.com>
2021-07-10 00:33:03 -0500

    ./*.*

--- dist/js/modo-obscuro.js
+++ dist/js/modo-obscuro.js
@@ -1,11 +1,22 @@
-const checkbox = document.querySelector('#toggle');
-const html = document.querySelector('html');
+// const checkbox = document.querySelector('#toggle');
+// const html = document.querySelector('html');
 
-const toggleDarkMode = function () {
-    checkbox.checked
-    ? html.classList.add('dark')
-    : html.classList.remove('dark')
-};
+// const toggleDarkMode = function () {
+//     checkbox.checked
+//     ? html.classList.add('dark')
+//     : html.classList.remove('dark')
+// };
 
-toggleDarkMode();
-checkbox.addEventListener('click', toggleDarkMode);
+// toggleDarkMode();
+// checkbox.addEventListener('click', toggleDarkMode);
+
+document.getElementById('toggle').addEventListener('click', function() {
+    let html = document.querySelector('html').classList;
+    if(localStorage.theme == 'dark') {
+        html.remove('dark');
+        localStorage.removeItem('theme')
+    } else {
+        html.add('dark');
+        localStorage.theme = 'dark';
+    }
+});
